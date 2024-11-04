819d44ae3ad258d5ca6dd9f498eb1ff04915c3cf
mirinai <tmddjs1771@gmail.com>
2024-11-04 11:35:16 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,14 @@
-import Bootstrap from './pages/Bootstrap';
-import Icon from './pages/Icon';
-import Style from './pages/Style';
-import UsingIcon from './pages/UsingIcon';
-import UsingIconWithCSSClass from './pages/UsingIconWithCSSClass';
-
 import './index.css';
 import './App.css';
+import Tailwindcss from './pages/Tailwindcss';
+import Color from './pages/Color';
 
 function App() {
   return (
     <div>
-      <UsingIconWithCSSClass />
-      <UsingIcon />
-      <Style />
-      <Icon />
-      <Bootstrap />
+      {/* <TextTest /> */}
+      <Color />
+      <Tailwindcss />
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {}
+  },
+  safelist: [{pattern: /^line-clamp-(\d+)$/}],
+  plugins: [require('@tailwindcss/line-clamp'), require('daisyui')]
+};
+// content: ['./src/**/*.{js,jsx,ts,tsx}'],
